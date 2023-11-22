4e9ba394046781c6aa7544eacc60abc919c5abed
Furkan Erdogan <furkanerdogan@outlook.com.tr>
2023-11-22 17:44:44 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MultiSelect from "./components/ui/multi-select";
+import classes from "./styles.module.scss";
+
+function App() {
+  return (
+    <div className={classes.wrapper}>
+      <MultiSelect />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/multi-select/index.tsx
@@ -0,0 +1,7 @@
+const MultiSelect = () => {
+  return (
+    <div>MultiSelect</div>
+  )
+}
+
+export default MultiSelect
