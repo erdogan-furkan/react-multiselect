ca984573c3ad6bae1a491749c729cf45e1661d73
Furkan Erdogan <furkanerdogan@outlook.com.tr>
2023-11-22 23:45:20 +0300

    Add toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import MultiSelect from "./components/ui/multi-select";
 import classes from "./styles.module.scss";
 
 function App() {
+  const data = [
+    "Edebiyat &amp; Romanlar",
+    "Gerilim filmleri",
+    "Çocuk kitapları",
+  ]
+
+  const regulatedData = data.map(item => ({value: item, label: item}))
+
   return (
     <div className={classes.wrapper}>
-      <MultiSelect />
+      <MultiSelect items={regulatedData} />
     </div>
   );
 }
--- src/components/ui/multi-select/index.tsx
+++ src/components/ui/multi-select/index.tsx
@@ -1,7 +1,57 @@
-const MultiSelect = () => {
-  return (
-    <div>MultiSelect</div>
-  )
+import { FC, useState } from "react";
+
+type Item = {
+  value: string;
+  label: string;
+};
+
+interface Props {
+  items: Array<Item>;
 }
 
-export default MultiSelect
+const MultiSelect: FC<Props> = ({ items }) => {
+  const [selectedItems, setSelectedItems] = useState<Array<Item["value"]>>([]);
+
+  const handleOnClickItem = (value: Item["value"]) => [
+    setSelectedItems((currentSelectedItems) => {
+      const currentItemIndex = currentSelectedItems.indexOf(value);
+
+      if (currentItemIndex === -1) {
+        currentSelectedItems.push(value);
+      } else {
+        currentSelectedItems.splice(currentItemIndex, 1);
+      }
+
+      return currentSelectedItems;
+    }),
+  ];
+
+  const sortedItems = items.sort((a, b) => {
+    if (selectedItems.includes(a.value) || selectedItems.includes(b.value))
+      return -1;
+    else return 1;
+  });
+
+  const renderItems = sortedItems.map((item) => {
+    const isSelectedItem = selectedItems.includes(item.value);
+
+    return (
+      <li
+        key={item.value}
+        className={`${isSelectedItem && "selected"}`}
+        onClick={handleOnClickItem.bind(this, item.value)}
+      >
+        <span></span>
+        <p>{item.label}</p>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <ul>{renderItems}</ul>
+    </div>
+  );
+};
+
+export default MultiSelect;
