49ae6663cda12271fc26f49f6a7992a897805fc0
Furkan Erdogan <furkanerdogan@outlook.com.tr>
2023-11-23 03:46:23 +0300

    Bind data to multi select

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import MultiSelect from "@/components/ui/multi-select";
 import classes from "./styles.module.scss";
+import { useEffect, useState } from "react";
 
 function App() {
-  const data = [
-    "Edebiyat &amp; Romanlar",
-    "Gerilim filmleri",
-    "Çocuk kitapları",
-  ];
+  const [data, setData] = useState<string[]>();
 
-  const regulatedData = data.map((item) => ({ value: item, label: item }));
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/items.json");
+
+      if (!response.ok) {
+        return window.alert("Bir hata ile karşılaşıldı!");
+      }
+
+      const data: { data: string[] } = await response.json();
+
+      setData(data.data);
+    })();
+  }, []);
+
+  const regulatedData = data?.map((item) => ({ value: item, label: item }));
 
   return (
     <div className={classes.wrapper}>
-      <MultiSelect items={regulatedData} title="Kategoriler" />
+      <MultiSelect items={regulatedData ?? []} title="Kategoriler" />
     </div>
   );
 }
